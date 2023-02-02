aa8d5eea33d9379773e57a2634a464561ce71760
yaolya <galyagina.olya@gmail.com>
2023-02-02 14:12:41 +0300

    Initial Commit

---
+++ ios-are-you-bored/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-are-you-bored
+//
+//  Created by Оля Галягина on 02.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
